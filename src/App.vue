60d3ac65a9ae18aa4a492420db170bdc21914c75
Sarvess Veeriyah <sarvess.jr@outlook.com>
2022-09-03 21:39:41 +0800

    main

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, computed} from 'vue'
+
+
+const player = ref('X');
+const board = ref([
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+])
+
+
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style>
+</style>
